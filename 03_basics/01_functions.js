f95d8ca964f791f5f07ac47c88019e5058e4bca2
Karuna Patel <karuna.bluedigital@gmail.com>
2026-03-31 19:35:29 +0530

    functions summmary

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -31,5 +31,28 @@ function loginUserMessage(username){
     return `${username} just logged in!`
 }
 // console.log(loginUserMessage("Karuna"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
 
+// function calculationCardPrice(...num1){ // ...num is rest operator
+//     return num1
+// }
+// console.log(calculationCardPrice(100, 200, 300, 400)); // [ 100, 200, 300, 400 ]
+
+function calculationCardPrice2(val1, val2,...num1){ // ...num is rest operator
+    return num1
+}
+
+// console.log(calculationCardPrice2(100, 200, 300, 400)); // [ 300, 400 ]
+
+
+const userData =  {
+    name:"Karunna Patel",
+    email:"karuna@gmail.com",
+    mobile:8767564570
+}
+
+function userLogin(anyobject){
+    // console.log(`${anyobject.name} has been logged in with ${anyobject.email}`)
+}
+
+userLogin(userData)
